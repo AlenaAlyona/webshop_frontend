4bcafc30ac4421c8ffd8ba297ace4d3fa2123f38
simottardi <simottardi@gmail.com>
2020-09-25 15:32:04 +0200

    mapped products

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -6,6 +6,7 @@ import { selectProduct } from "../store/product/selectors";
 
 export default function HomePage() {
   const products = useSelector(selectProduct);
+  console.log("selected products", products);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -14,20 +15,18 @@ export default function HomePage() {
 
   return (
     <div>
-      <p>I am a proud HOMEPAGE</p>
-      <p>
-        {/* 
-        {data.loading ? ({ // data not in React state anymore */}
-        {products ? (
-          <em>Loading...</em>
-        ) : (
-          /* <button onClick={fetchNext5Posts}>Load more</button> //  the action now is managed by an action dispatcher as below*/
+      <h1>Welcome to our fancy webshop!</h1>
 
-          <button /* onClick={() => dispatch(fetchNext5Posts)} */>
-            Load more
-          </button> // we use an anonymouse function to call dispach the action
-        )}
-      </p>
+      {products.map((product) => {
+        return (
+          <div key={product.id}>
+            <h3>{product.name}</h3>
+            {/* <Link to={`/Post/${product.id}`}>See product details</Link> */}
+            <p>€ {product.price}</p>
+            <p>{product.description}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
